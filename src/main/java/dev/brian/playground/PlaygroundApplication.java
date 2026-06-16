63582045a4c284f14d0919ba96a9ff6b9548f4a0
Brian Ricardo Tamin <19623154@mahasiswa.itb.ac.id>
2026-06-16 23:14:54 +0700

    springboot init

---
+++ src/main/java/dev/brian/playground/PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package dev.brian.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
+
+}
