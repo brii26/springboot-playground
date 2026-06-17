3ae6afe3437ee2e421946c11ed885910b768f227
Brian Ricardo Tamin <19623154@mahasiswa.itb.ac.id>
2026-06-17 23:27:41 +0700

    feat: add user repository

---
+++ src/main/java/dev/brian/playground/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.brian.playground.repository;
+
+import dev.brian.playground.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByEmail(String Email);
+}
