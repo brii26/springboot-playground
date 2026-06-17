f2818193a1d8ad208a72824b4eefa3e330f6b7ad
Brian Ricardo Tamin <19623154@mahasiswa.itb.ac.id>
2026-06-17 12:55:49 +0700

    feat(db) : setup database & make entities"

---
+++ src/main/java/dev/brian/playground/entity/Post.java
@@ -0,0 +1,31 @@
+package dev.brian.playground.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    private Boolean published = false;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/dev/brian/playground/entity/User.java
@@ -0,0 +1,32 @@
+package dev.brian.playground.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	private String name;
+
+	@OneToMany(mappedBy = "author")
+	private List<Post> posts;
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+}
